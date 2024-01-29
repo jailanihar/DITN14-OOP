d52761d3302e9ea256dac107189571ca33548439
Jailani Abdul Rahman <jailani.rahman@pb.edu.bn>
2024-01-29 13:18:15 +0800

    Week2 slot1 end class source code

--- week02/slot01/app/TestApp.java
+++ week02/slot01/app/TestApp.java
@@ -5,12 +5,12 @@ import week02.slot01.assets.Pirate;
 public class TestApp {
 
 	public static void main(String[] args) {
-		Pirate pirate1 = new Pirate("Abu", 4);
-		Pirate pirate2 = new Pirate("Bakar", 3);
+		Pirate pirate1 = new Pirate(new String(""), -5);
+		Pirate pirate2 = new Pirate(null, 3);
 		Pirate pirate3 = new Pirate("Curi", 5);
-		System.out.println(pirate1.name);
-		System.out.println(pirate2.name);
-		System.out.println(pirate3.name);
+		System.out.println(pirate1);
+		System.out.println(pirate2);
+		System.out.println(pirate3);
 	}
 	
 }
--- week02/slot01/assets/Pirate.java
+++ week02/slot01/assets/Pirate.java
@@ -2,13 +2,26 @@ package week02.slot01.assets;
 
 public class Pirate {
 
-	public static int totalPirates = 0;
-	public String name;
-	int health;
+	private final static String DEFAULT_NAME_PREFIX = "Pirate";
+	private final static int DEFAULT_HEALTH = 5;
+	private static int totalDefaultPirates = 1;
+	private String name;
+	private int health;
 	
 	public Pirate(String pName, int pHealth) {
+		if(pName == null || pName.equals("")) {
+			pName = DEFAULT_NAME_PREFIX + totalDefaultPirates;
+			totalDefaultPirates++;
+		}
 		name = pName;
+		if(pHealth < 1) {
+			pHealth = DEFAULT_HEALTH;
+		}
 		health = pHealth;
+	}
+	
+	public String toString() {
+		return "[" + name + ", " + health + "]";
 	}
 	
 }
