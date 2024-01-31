2011e07b8db975b1e1fa3a71084fc547e3b177d5
Jailani Abdul Rahman <jailani.rahman@pb.edu.bn>
2024-01-31 10:33:39 +0800

    Week02 slot02 final code

--- week02/slot02/app/TestApp.java
+++ week02/slot02/app/TestApp.java
@@ -6,24 +6,30 @@ import week02.slot02.assets.Pirate;
 public class TestApp {
 
 	public static void main(String[] args) {
-		Map map = new Map();
+		Map map = new Map(5, 5);
 
-		map.arena[1][2] = new Pirate("Abu", 4);
-		map.arena[0][0] = new Pirate("Bakar", 3);
-		map.arena[4][3] = new Pirate("Curi", 5);
-		map.arena[2][4] = new Pirate("Daging", 2);
+//		map.arena[1][2] = new Pirate("Abu", 4);
+		map.addPirate(1, 2, new Pirate("Abu", 4));
+//		map.arena[0][0] = new Pirate("Bakar", 3);
+		map.addPirate(0, 0, new Pirate("Bakar", 3));
+//		map.arena[4][3] = new Pirate("Curi", 5);
+		map.addPirate(4, 3, new Pirate("Curi", 5));
+//		map.arena[2][4] = new Pirate("Daging", 2);
+		map.addPirate(2, 4, new Pirate("Daging", 2));
 
 		printArena(map);
 
-		map.arena[1][0] = new Pirate("Emak", 6);
+//		map.arena[1][0] = new Pirate("Emak", 6);
+		map.addPirate(-1, 0, new Pirate("Emak", 6));
 
 		printArena(map);
+		System.out.println(map.selectPirate(1, 5));
 	}
 
 	public static void printArena(Map map) {
-		for(int y = 0; y < map.arena[0].length; y++) {
-			for(int x = 0; x < map.arena.length; x++) {
-				System.out.print(map.arena[x][y] + "\t");
+		for(int y = 0; y < map.getHeight(); y++) {
+			for(int x = 0; x < map.getWidth(); x++) {
+				System.out.print(map.selectPirate(x, y) + "\t");
 			}
 			System.out.println();
 		}
--- week02/slot02/assets/Map.java
+++ week02/slot02/assets/Map.java
@@ -3,6 +3,45 @@ package week02.slot02.assets;
 public class Map {
 
 	// Pirate[width or x][height or y] 
-	public Pirate[][] arena = new Pirate[5][6];
+	private Pirate[][] arena;
+	
+	public Map(int width, int height) {
+		if(width < 1) {
+			width = 5;
+		}
+		if(height < 1) {
+			height = 5;
+		}
+		arena = new Pirate[width][height];
+	}
+	
+	public int getHeight() {
+		return arena[0].length;
+	}
+	
+	public int getWidth() {
+		return arena.length;
+	}
+	
+	public Pirate selectPirate(int x, int y) {
+		if(checkCoordinate(x, y)) {
+			return arena[x][y];
+		}
+		return null;
+	}
+	
+	public void addPirate(int x, int y, Pirate newPirate) {
+		if(checkCoordinate(x, y)) {
+			arena[x][y] = newPirate;
+		}
+	}
+	
+	public boolean checkCoordinate(int x, int y) {
+		if(x < 0 || x >= getWidth() || y < 0 || y >= getHeight()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 	
 }
